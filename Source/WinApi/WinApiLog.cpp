e878fca36e1dd7cf9f8f878f9b72aea700fdafe4
NebulousDev <nebulousdev@gmail.com>
2022-06-27 01:38:35 -0400

    Redesigned log tests + formatting

--- Source/WinApi/WinApiLog.cpp
+++ Source/WinApi/WinApiLog.cpp
@@ -11,28 +11,28 @@
 
 namespace Quartz
 {
-	WORD sWin32DefaultLogColors = 0;
-	FILE* spWin32OutputStream = 0;
+	WORD sDefaultLogColors = 0;
+	FILE* spOutputStream = 0;
 
 	WORD sWin32LogColors[] =
 	{
-		/* LOG_COLOR_DEFAULT */			0,
-		/* LOG_COLOR_BLACK */			0x0,
-		/* LOG_COLOR_DARK_BLUE */		FOREGROUND_BLUE,
-		/* LOG_COLOR_DARK_GREEN */		FOREGROUND_GREEN,
-		/* LOG_COLOR_DARK_CYAN */		FOREGROUND_GREEN | FOREGROUND_BLUE,
-		/* LOG_COLOR_DARK_RED */		FOREGROUND_RED,
-		/* LOG_COLOR_DARK_MAGENTA */	FOREGROUND_RED | FOREGROUND_BLUE,
-		/* LOG_COLOR_DARK_YELLOW */		FOREGROUND_RED | FOREGROUND_GREEN,
-		/* LOG_COLOR_DARK_GRAY */		0x8,
-		/* LOG_COLOR_GRAY */			0x7,
-		/* LOG_COLOR_BLUE */			FOREGROUND_BLUE | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_GREEN */			FOREGROUND_GREEN | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_CYAN */			FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_RED */				FOREGROUND_RED | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_MAGENTA */			FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_YELLOW */			FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
-		/* LOG_COLOR_WHITE */			FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_DEFAULT */         0,
+		/* LOG_COLOR_BLACK */           0x0,
+		/* LOG_COLOR_DARK_BLUE */       FOREGROUND_BLUE,
+		/* LOG_COLOR_DARK_GREEN */      FOREGROUND_GREEN,
+		/* LOG_COLOR_DARK_CYAN */       FOREGROUND_GREEN | FOREGROUND_BLUE,
+		/* LOG_COLOR_DARK_RED */        FOREGROUND_RED,
+		/* LOG_COLOR_DARK_MAGENTA */    FOREGROUND_RED | FOREGROUND_BLUE,
+		/* LOG_COLOR_DARK_YELLOW */     FOREGROUND_RED | FOREGROUND_GREEN,
+		/* LOG_COLOR_DARK_GRAY */       0x8,
+		/* LOG_COLOR_GRAY */            0x7,
+		/* LOG_COLOR_BLUE */            FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_GREEN */           FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_CYAN */            FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_RED */             FOREGROUND_RED | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_MAGENTA */         FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_YELLOW */          FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+		/* LOG_COLOR_WHITE */           FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
 	};
 
 	void Log::InitLogging()
@@ -51,8 +51,8 @@ namespace Quartz
 		setvbuf(pStream, NULL, _IONBF, 1);
 		_setmode(_fileno(pStream), _O_U16TEXT);
 
-		sWin32DefaultLogColors = consoleInfo.wAttributes;
-		spWin32OutputStream = pStream;
+		sDefaultLogColors = consoleInfo.wAttributes;
+		spOutputStream = pStream;
 
 		SetLogLevel((LogLevel)QUARTZ_LOG_LEVEL);
 	}
@@ -73,8 +73,8 @@ namespace Quartz
 
 		sprintf(prefixedFormat, "[%s][%s] %s\n", currentTime, severityName, format);
 
-		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0x0F : sWin32LogColors[foreground];
-		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
+		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0x0F : sWin32LogColors[foreground];
+		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(hConsole, forgroundColor | backgroundColor);
 
@@ -95,11 +95,11 @@ namespace Quartz
 
 		MultiByteToWideChar(CP_UTF8, 0, pBuffer, bufferSize, pWideBuffer, wideBufferSize);
 
-		fwprintf(spWin32OutputStream, pWideBuffer);
+		fwprintf(spOutputStream, pWideBuffer);
 
 		va_end(args);
 
-		SetConsoleTextAttribute(hConsole, sWin32DefaultLogColors);
+		SetConsoleTextAttribute(hConsole, sDefaultLogColors);
 
 		delete[] pBuffer;
 		delete[] pWideBuffer;
@@ -120,25 +120,25 @@ namespace Quartz
 
 		swprintf(prefixedFormat, L"[%s][%s] %ls\n", currentTime, severityName, format);
 
-		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0x0F : sWin32LogColors[foreground];
-		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
+		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0x0F : sWin32LogColors[foreground];
+		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(hConsole, forgroundColor | backgroundColor);
 
 		va_list args;
 		va_start(args, format);
 
-		vfwprintf(spWin32OutputStream, prefixedFormat, args);
+		vfwprintf(spOutputStream, prefixedFormat, args);
 
 		va_end(args);
 
-		SetConsoleTextAttribute(hConsole, sWin32DefaultLogColors);
+		SetConsoleTextAttribute(hConsole, sDefaultLogColors);
 	}
 
 	void Log::_LogRaw(LogColor foreground, LogColor background, const char* format, ...)
 	{
-		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0x0F : sWin32LogColors[foreground];
-		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
+		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0x0F : sWin32LogColors[foreground];
+		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(hConsole, forgroundColor | backgroundColor);
 
@@ -159,11 +159,11 @@ namespace Quartz
 
 		MultiByteToWideChar(CP_UTF8, 0, pBuffer, bufferSize, pWideBuffer, wideBufferSize);
 
-		fwprintf(spWin32OutputStream, pWideBuffer);
+		fwprintf(spOutputStream, pWideBuffer);
 
 		va_end(args);
 
-		SetConsoleTextAttribute(hConsole, sWin32DefaultLogColors);
+		SetConsoleTextAttribute(hConsole, sDefaultLogColors);
 
 		delete[] pBuffer;
 		delete[] pWideBuffer;
@@ -171,18 +171,18 @@ namespace Quartz
 
 	void Log::_LogRaw(LogColor foreground, LogColor background, const wchar_t* format, ...)
 	{
-		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0x0F : sWin32LogColors[foreground];
-		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sWin32DefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
+		WORD forgroundColor = (foreground == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0x0F : sWin32LogColors[foreground];
+		WORD backgroundColor = (background == LOG_COLOR_DEFAULT) ? sDefaultLogColors & 0xF0 : (sWin32LogColors[background] << 4) & 0xF0;
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(hConsole, forgroundColor | backgroundColor);
 
 		va_list args;
 		va_start(args, format);
 
-		vfwprintf(spWin32OutputStream, format, args);
+		vfwprintf(spOutputStream, format, args);
 
 		va_end(args);
 
-		SetConsoleTextAttribute(hConsole, sWin32DefaultLogColors);
+		SetConsoleTextAttribute(hConsole, sDefaultLogColors);
 	}
 }
